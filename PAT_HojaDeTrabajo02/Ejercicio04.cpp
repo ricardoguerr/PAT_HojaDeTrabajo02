17669d75684d52cd33d1c90ee705efb47be66cde
ricardoguerr <123261362+ricardoguerr@users.noreply.github.com>
2023-11-02 19:49:14 -0600

    Update Ejercicio04.cpp

--- PAT_HojaDeTrabajo02/Ejercicio04.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio04.cpp
@@ -1,32 +1,47 @@
 #include "Ejercicio04.h"
 
-FrontMiddleBackQueue::FrontMiddleBackQueue()
-{
+FrontMiddleBackQueue::FrontMiddleBackQueue() {
 }
 
-void FrontMiddleBackQueue::pushFront(int value)
-{
+void FrontMiddleBackQueue::pushFront(int value) {
+    data.push_front(value);
 }
 
-void FrontMiddleBackQueue::pushMiddle(int value)
-{
+void FrontMiddleBackQueue::pushMiddle(int value) {
+    auto it = data.begin();
+    std::advance(it, data.size() / 2);
+    data.insert(it, value);
 }
 
-void FrontMiddleBackQueue::pushBack(int value)
-{
+void FrontMiddleBackQueue::pushBack(int value) {
+    data.push_back(value);
 }
 
-int FrontMiddleBackQueue::popFront()
-{
-	return 0;
+int FrontMiddleBackQueue::popFront() {
+    if (data.empty()) {
+        return -1;
+    }
+    int front = data.front();
+    data.pop_front();
+    return front;
 }
 
-int FrontMiddleBackQueue::popMiddle()
-{
-	return 0;
+int FrontMiddleBackQueue::popMiddle() {
+    if (data.empty()) {
+        return -1;
+    }
+    auto it = data.begin();
+    std::advance(it, (data.size() - 1) / 2);
+    int middle = *it;
+    data.erase(it);
+    return middle;
 }
 
-int FrontMiddleBackQueue::popBack()
-{
-	return 0;
+int FrontMiddleBackQueue::popBack() {
+    if (data.empty()) {
+        return -1;
+    }
+    int back = data.back();
+    data.pop_back();
+    return back;
 }
