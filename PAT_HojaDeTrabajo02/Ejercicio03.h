80bafd86908bb2aa39d79030d66fba18baecf316
ricardoguerr <123261362+ricardoguerr@users.noreply.github.com>
2023-11-02 19:48:46 -0600

    Update Ejercicio03.h

--- PAT_HojaDeTrabajo02/Ejercicio03.h
+++ PAT_HojaDeTrabajo02/Ejercicio03.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <stack>
 
 using std::string;
 
@@ -8,5 +9,5 @@ class Ejercicio03
 {
 public:
 	int calculate(string s);
+	int applyOperation(int a, int b, char op);
 };
-
