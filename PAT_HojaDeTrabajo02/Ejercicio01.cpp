fccd62f82d628d045c83af515182f871b98a8e3b
ricardoguerr <123261362+ricardoguerr@users.noreply.github.com>
2023-11-02 19:46:45 -0600

    Update Ejercicio01.cpp

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -2,5 +2,33 @@
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    return nullptr;
+    if (!head || k == 0) {
+        return head;
+    }
+
+    int length = 1;
+    Node<char>* tail = head;
+    while (tail->next) {
+        tail = tail->next;
+        length++;
+    }
+
+    k = k % length;
+
+    if (k == 0) {
+        return head;
+    }
+
+    Node<char>* newHead = head;
+    Node<char>* newTail = head;
+    for (int i = 0; i < length - k - 1; i++) {
+        newTail = newTail->next;
+    }
+
+    newHead = newTail->next;
+
+    newTail->next = nullptr;
+    tail->next = head;
+
+    return newHead;
 }
